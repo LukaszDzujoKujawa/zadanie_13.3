e30246bee71017c3cda2491ec5fe5eb868a32589
LukaszDzujoKujawa <35543753+LukaszDzujoKujawa@users.noreply.github.com>
2018-06-06 22:08:03 +0200

    Update task.js

--- js/task.js
+++ js/task.js
@@ -2,20 +2,21 @@ process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
-    if (input !== null) {
-        var instruction = input.toString().trim();
-  
-      switch(instruction) {
+    if (!input) {
+      return;
+    }
+     
+    var instruction = input.toString().trim();
+
+    switch(instruction) {
       case '/exit':
         process.stdout.write('Quitting app!\n');
         process.exit();
         break;
-      
       case '/sayhello':
-          process.stdout.write('hello!\n');
-           break;
+        process.stdout.write('hello!\n');
+        break;
       default:
-          process.stderr.write('Wrong instruction!\n');
-        };
-    }
+        process.stderr.write('Wrong instruction!\n');
+    }; 
 });
